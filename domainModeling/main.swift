849551c9ebce40da08a7d715fc0b72479c8303c3
Lijuan Zhang <zhanglj@uw.edu>
2015-10-15 15:37:46 -0700

    Initial Commit

---
+++ domainModeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domainModeling
+//
+//  Created by Keran Zheng on 10/15/15.
+//  Copyright © 2015 Lijuan Zhang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
