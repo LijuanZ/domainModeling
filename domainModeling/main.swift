5e9b908b57063aec20889470b2975e99d2ce777c
Lijuan Zhang <zhanglj@uw.edu>
2015-10-15 15:39:27 -0700

    First commit

--- domainModeling/main.swift
+++ domainModeling/main.swift
@@ -1,12 +1,282 @@
 //
 //  main.swift
-//  domainModeling
+//  domain-modeling
 //
-//  Created by Keran Zheng on 10/15/15.
+//  Created by Lijuan Zhang on 10/14/15.
 //  Copyright © 2015 Lijuan Zhang. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+enum Currency {
+    case USD, GBP, EUR, CAN
+}
+
+struct Money {
+    var amount: Double
+    var currency: Currency
+    
+    init(amount: Double, currency: Currency) {
+        self.amount = amount
+        self.currency = currency
+    }
+    
+    //Stringify amount and currency properties in order to print
+    var stringified: String {
+        get {
+            let cur: String
+            switch currency {
+            case .USD: cur = "USD"
+            case .GBP: cur = "GBP"
+            case .EUR: cur = "EUR"
+            case .CAN: cur = "CAN"
+            }
+            
+            return "\(amount) " + cur
+        }
+    }
+    
+    func convert(toCurrency: Currency) -> Money {
+        /*exchangeRate stores exchange rates from USD to other currencies. For example, exchangeRate[Currency.GBP] is the rate to convert USD to GBP,
+        so the rate to change GBP to USD is 1/exchangeRate[Currency.GBP]!
+        */
+        let exchangeRate: Dictionary<Currency, Double> = [Currency.USD: 1, Currency.GBP: 0.5, Currency.EUR: 1.5, Currency.CAN: 1.25]
+        
+        //When converting, convert the currency to USD, and then convert from USD to target currency
+        let amountAfterConvert = self.amount * (1/exchangeRate[self.currency]!) * (exchangeRate[toCurrency]!)
+        return Money(amount: amountAfterConvert, currency: toCurrency)
+    }
+    
+    //When adding different currencies, will convert them to the most left currency
+    static func add(moneys: Money...) -> Money {
+        let outCurrency: Currency = moneys[0].currency
+        var outputAmount: Double = 0.0
+        for money in moneys {
+            if money.currency != outCurrency {
+                let convertedMoney = money.convert(outCurrency)
+                outputAmount += convertedMoney.amount
+            }
+            else {
+                outputAmount += money.amount
+            }
+        }
+        return Money(amount: outputAmount, currency: outCurrency)
+    }
+    
+    //When subtracting different currencies, will convert them to the most left currency
+    static func subtract(moneys: Money...) -> Money {
+        let outCurrency: Currency = moneys[0].currency
+        var outputAmount: Double = moneys[0].amount
+        for var i = 1; i < moneys.count; i++ {
+            let money = moneys[i]
+            if money.currency != outCurrency {
+                let convertedMoney = money.convert(outCurrency)
+                outputAmount -= convertedMoney.amount
+            }
+            else {
+                outputAmount -= money.amount
+            }
+        }
+        return Money(amount: outputAmount, currency: outCurrency)
+    }
+}
+
+//Test of Money
+print("Test of Money")
+let money = Money(amount: 10, currency: Currency.GBP)
+let convertToUSD = money.convert(Currency.USD)
+print("Convert 10 GBP to USD: " + convertToUSD.stringified)
+let convertToCAN = money.convert(Currency.CAN)
+print("Convert 10 GBP to CAN: " + convertToCAN.stringified)
+print("20 USD + 10GBP: " + Money.add(money, convertToUSD).stringified)
+let moneyLeft = Money(amount: 50, currency: Currency.EUR)
+print("50 EUR - 10 GBP: " + Money.subtract(moneyLeft, money).stringified)
+print("50 EUR - 20 EUR: " + Money.subtract(moneyLeft, Money(amount: 20, currency: Currency.EUR)).stringified)
+print("50 EUR - 20 EUR - 20 EUR: " + Money.subtract(moneyLeft, Money(amount: 20, currency: Currency.EUR), Money(amount: 20, currency: Currency.EUR)).stringified)
+
+//Job
+class Job {
+    var title: String
+    var salary: Money
+    var isSalaryPerHour: Bool
+    
+    init(title: String, salary: Money, isSalaryPerHour: Bool) {
+        self.title = title
+        self.salary = salary
+        self.isSalaryPerHour = isSalaryPerHour
+    }
+    
+    func calculateIncome(numberOfHours: Double) -> Money {
+        if (self.isSalaryPerHour) {
+            let income = Money(amount: salary.amount * numberOfHours, currency: salary.currency)
+            return income
+        }
+        return salary
+    }
+    
+    func raise(raisePercent: Double) -> Money {
+        salary.amount = salary.amount * (1 + raisePercent * 0.01)
+        return salary
+    }
+}
+
+//Test of Job
+print("\nTest of Job")
+print("Generate a testJob with title: 'SDE', salary: 100000 USD and the salary is per year.")
+let testJob = Job(title: "SDE", salary: Money(amount: 100000, currency: Currency.USD), isSalaryPerHour: false)
+print("testJob.calculateIncome(2080): \(testJob.calculateIncome(2080).stringified)")
+print("Salary of testJob after raise 20%: \(testJob.raise(20).stringified)")
+print("Generate a testJob2 with title: 'SDE', salary: 40 USD and the salary is per hour.")
+let testJob2 = Job(title: "UX Designer", salary: Money(amount: 40, currency: Currency.USD), isSalaryPerHour: true)
+print("testJob.calculateIncome(2080): \(testJob2.calculateIncome(2080).stringified)")
+print("Salary of testJob after raise 20%: \(testJob2.raise(20).stringified)")
+
+//Person
+class Person {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    var job: Job?
+    var spouse: Person?
+    
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.job = nil
+        self.spouse = nil
+    }
+    
+    init(firstName: String, lastName: String, age: Int, job: Job?, spouse: Person?) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        
+        if age < 16 {
+            self.job = nil
+        }
+        else {
+            self.job = job
+        }
+        
+        if age < 18 {
+            self.spouse = nil
+        }
+        else {
+            self.spouse = spouse
+        }
+    }
+    
+    func toString() -> String {
+        var selfString = "firstName: " + firstName + ", lastName: " + lastName + ", age: \(age)"
+        
+        if job != nil {
+            selfString = selfString + ", jobTitle: " + job!.title + ", jobSalary: " + job!.salary.stringified + (job!.isSalaryPerHour ? " per hour" : " per year")
+        }
+        else {
+            selfString += ", job: nil"
+        }
+        
+        if spouse != nil {
+            selfString += ", spouseFirstName: " + spouse!.firstName + ", spouseLastName: " + spouse!.lastName
+        }
+        else {
+            selfString += ", spouse: nil"
+        }
+        
+        return selfString
+    }
+}
+
+//Test of Person
+print("\nTest of Person")
+print("Generate a testPerson without job and spouse. Age: 16.")
+let testPerson = Person(firstName: "Jane", lastName: "Doe", age: 16)
+print("testPerson.toString(): \(testPerson.toString())")
+
+print("Generate tow persons: Lily and Jim and they are spouse")
+let lily = Person(firstName: "Lily", lastName: "Joe", age: 25, job: testJob2, spouse: nil)
+let jim = Person(firstName: "Jim", lastName: "Green", age: 28, job: testJob, spouse: lily)
+lily.spouse = jim
+print("lily.toString(): \(lily.toString())")
+print("jim.toString(): \(jim.toString())")
+
+
+//Family
+enum FamilyInitError: ErrorType{
+    case IllegalInitilization
+}
+
+class Family {
+    var members: [Person]
+    
+    init(members: [Person]) throws {
+        var isLegal: Bool = false
+        for member in members {
+            if member.age >= 21 {
+                isLegal = true
+            }
+        }
+        self.members = members
+        
+        if !isLegal {
+            print("No person is over age 21, illegal.")
+            throw FamilyInitError.IllegalInitilization
+        }
+    }
+    
+    //In order to calculate householdIncome, working hours of each member are needed. members who do not have a job has 0 working hour
+    func householdIncome(workingHours: [Double]) -> Money {
+        var combinedIncome: Money = Money(amount: 0, currency: Currency.USD)
+        
+        for var i = 0; i < members.count; i++ {
+            let member = members[i]
+            if member.job != nil {
+                combinedIncome = Money.add(combinedIncome, member.job!.calculateIncome(workingHours[i]))
+            }
+        }
+        
+        return combinedIncome
+    }
+    
+    func haveChild(firstName: String, lastName: String) -> Void {
+        let child = Person(firstName: firstName, lastName: lastName, age: 0)
+        self.members.append(child)
+    }
+    
+    //return family members' firstName and lastName
+    func displayMembers() -> Void {
+        for member in members {
+            print(member.toString())
+        }
+    }
+}
+
+print("\nTest of Family")
+print("Generating a family with a person of age 5: ")
+let child = Person(firstName: "Jhon", lastName: "Doe", age: 5)
+do {
+    var family = try Family(members: [child])
+}
+catch FamilyInitError.IllegalInitilization{
+    print("Must have at least one person over age 21 to create a family")
+}
+
+print("\nGenerating a family with Lily and Jim: ")
+do {
+    var family = try Family(members: [lily, jim])
+    print("householdIncome while both of them work 2080 hours per year: " + family.householdIncome([2080, 2080]).stringified)
+    print("family members: ")
+    family.displayMembers()
+    //Add child
+    family.haveChild("Laura", lastName: "Green")
+    print("\nfamily members after have a child: ")
+    family.displayMembers()
+}
+catch FamilyInitError.IllegalInitilization{
+    print("Must have at least one person over age 21 to create a family")
+}
+
+
+
 
